98fc54c5b1867fe979fa80c2a0cc674df52e3c16
destynurulanitsa <destynurulanitsa24@gmail.com>
2022-03-28 15:05:49 +0700

    memastikan

--- hapus.php
+++ hapus.php
@@ -1,16 +1,19 @@
-<?php 
-// echo $_GET["nim"];
+<?php
+//echo $_GET["nim"];
 require_once('koneksi.php');
 
-$sql = "delete from mahasiswanwhre nim= '" . $_GET['nim'] . "'" ;
+$sql = "delete from mahasiswa where nim='" . $_GET['nim'] . "'";
 $result = pg_query($sql);
-$jumlah = pg_num_rows(result);
 
-if ($jumlah > 0){
-        echo '<script type="text/javascript">';
-        echo 'alert("Data telah tersimpan");';
-        echo 'window.location.href = "index.php";';
-        echo '</script>';
+if($result) {
+    echo '<script type="text/javascript">';
+    echo 'alert("Data telah terhapus");';
+    echo 'window.location.href = "index.php";';
+    echo '</script>';
+} else {
+    echo '<script type="text/javascript">';
+    echo 'alert("Data GAGAL terhapus");';
+    echo 'window.location.href = "index.php";';
+    echo '</script>';
 }
-        
 ?>
